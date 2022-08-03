e103a65ca0313c23b01f7a53f82fe29770d9ecc9
Alaric Pan <alaricpan@gmail.com>
2022-08-03 00:07:16 -0400

    successfully compiled

---
+++ src/bin/blink_example_attiny412pac.rs
@@ -0,0 +1,33 @@
+#![no_std]
+#![no_main]
+
+extern crate panic_halt;
+
+use megatiny_hal::Peripherals;
+use core::ops::Deref;
+
+#[no_mangle]
+pub extern fn main() {
+    let peripherals = unsafe {Peripherals::steal()};
+    let vporta = peripherals.VPORTA.deref();
+    let rtc = peripherals.RTC.deref();
+
+    vporta.dir.write(|w| unsafe {w.bits(0b1 << 3)});
+    loop {
+        vporta.out.modify(|r, w| unsafe {w.bits(r.bits() | 0b1 << 3)});
+        //delays 1 second
+        rtc.cnt.reset();
+        let mut start = rtc.cnt.read().bits();
+        while rtc.cnt.read().bits() - start < 1000 {
+            continue;
+        }
+
+        vporta.out.modify(|r, w| unsafe{w.bits(r.bits() & !(0b1 << 3))});
+        //delays 1 second
+        rtc.cnt.reset();
+        start = rtc.cnt.read().bits();
+        while rtc.cnt.read().bits() - start < 1000 {
+            continue;
+        }
+    }
+}
